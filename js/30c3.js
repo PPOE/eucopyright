24322a5c3a304b00f31f378ff58ddf90e86f08ec
PeterTheOne <petertheone@gmail.com>
2014-02-03 02:42:55 +0100

    fix bugs with pull request

--- js/30c3.js
+++ js/30c3.js
@@ -6,7 +6,8 @@ var guideSlug = 'crew_30c3';
 var problems2questions = {
   youtube: [1],
   imagesharing: [58],
-  sharingcriminalization: [23],
+  sharingcriminalization: [80],
+  filesharingconfusion: [80],
   drm: [64],
   resell: [13],
   mediacountry: [21],
@@ -18,7 +19,6 @@ var problems2questions = {
   affordmedia: [20],
   travel: [22],
   cclicense: [15],
-  filesharingconfusion: [80],
   enforcement: [77],
   links: [11],
   duration: [20]
